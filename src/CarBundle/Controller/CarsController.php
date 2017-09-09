da90950f2722290cc3335927655ca6915dc074ca
Igor Bzovsky <igor77188@gmail.com>
2017-09-09 17:11:53 +0300

    Add Bootstrap and Jquery

--- src/CarBundle/Controller/CarsController.php
+++ src/CarBundle/Controller/CarsController.php
@@ -4,6 +4,9 @@ namespace CarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CarsController extends Controller
 {
@@ -13,8 +16,6 @@ class CarsController extends Controller
     public function showAction()
     {
         return $this->render('CarBundle:Cars:show.html.twig', array(
-            // ...
         ));
     }
-
 }
